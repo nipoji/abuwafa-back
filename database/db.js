122b2155a70a652e9a1d60b8e4193ba885173e68
nipoji <hanifnurfauzi1@gmail.com>
2025-01-20 00:06:43 +0700

    Abuwafa API

--- database/db.js
+++ database/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 const fs = require('fs');
 
-const caPemPath = '../ca.pem';
+const caPemPath = './ca.pem';
 
 const pool = mysql.createPool({
   uri: process.env.DATABASE_URL,
