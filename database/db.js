54131e4e9d468d7624011420115605dcf36616a7
nipoji <hanifnurfauzi1@gmail.com>
2025-01-19 23:55:16 +0700

    Abuwafa API

--- database/db.js
+++ database/db.js
@@ -1,11 +1,14 @@
 // db.js
 const mysql = require('mysql2/promise');
 require('dotenv').config();
+const fs = require('fs');
+
+const caPemPath = '../ca.pem';
 
 const pool = mysql.createPool({
   uri: process.env.DATABASE_URL,
   ssl: {
-    ca: Buffer.from(process.env.DB_CA, 'utf-8'),
+    ca: fs.readFileSync(caPemPath),
     rejectUnauthorized: true
   },
   waitForConnections: true,
